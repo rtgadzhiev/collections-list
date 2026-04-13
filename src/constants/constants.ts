352dd072b31806034f527401b3fe8be2ca3bd8a7
Ruslan Gadzhiev <rtgadzhiev@gmail.com>
2026-04-13 14:18:26 +0400

    feat: add constants types

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,14 +4,33 @@ import telegramIcon from '../assets/images/icons/socials/telegram-icon.svg';
 import tiktokIcon from '../assets/images/icons/socials/tiktok-icon.svg';
 import youtubeIcon from '../assets/images/icons/socials/youtube-icon.svg';
 
-export const MENU_ITEMS = [
+interface IBaseItem {
+  id: number;
+  title: string;
+}
+
+interface IMenuItem extends IBaseItem {
+  isActive?: boolean;
+}
+
+type TMenuItems = IMenuItem[];
+
+export const MENU_ITEMS: TMenuItems = [
   { id: 1, title: 'База вопросов', isActive: true },
   { id: 2, title: 'Тренажер' },
   { id: 3, title: 'Материалы' },
   { id: 4, title: 'Навыки (hh)' },
 ];
 
-export const COMPLEXITY_OPTIONS = {
+interface IComplexityOption extends IBaseItem {
+  value: number[];
+}
+
+interface IComplexityOptions {
+  data: IComplexityOption[];
+}
+
+export const COMPLEXITY_OPTIONS: IComplexityOptions = {
   data: [
     { id: 1, title: '1-3', value: [1, 2, 3] },
     { id: 2, title: '4-6', value: [4, 5, 6] },
@@ -20,17 +39,27 @@ export const COMPLEXITY_OPTIONS = {
   ],
 };
 
-export const RATING_OPTIONS = {
+interface IRatingOptions {
+  data: IBaseItem[];
+}
+
+export const RATING_OPTIONS: IRatingOptions = {
   data: [
-    { id: 1, title: 1 },
-    { id: 2, title: 2 },
-    { id: 3, title: 3 },
-    { id: 4, title: 4 },
-    { id: 5, title: 5 },
+    { id: 1, title: '1' },
+    { id: 2, title: '2' },
+    { id: 3, title: '3' },
+    { id: 4, title: '4' },
+    { id: 5, title: '5' },
   ],
 };
 
-export const SOCIALS_ITEMS = [
+interface ISocialItem extends IBaseItem {
+  icon: string;
+}
+
+type TSocialItems = ISocialItem[];
+
+export const SOCIALS_ITEMS: TSocialItems = [
   { id: 1, title: 'Figma', icon: figmaIcon },
   { id: 2, title: 'Telegram', icon: telegramIcon },
   { id: 3, title: 'YouTube', icon: youtubeIcon },
@@ -38,4 +67,9 @@ export const SOCIALS_ITEMS = [
   { id: 5, title: 'GitHub', icon: githubIcon },
 ];
 
-export const TITLE_TAGS = { h1: 'h1', h2: 'h2' };
+interface ITitleTags {
+  h1: string;
+  h2: string;
+}
+
+export const TITLE_TAGS: ITitleTags = { h1: 'h1', h2: 'h2' };

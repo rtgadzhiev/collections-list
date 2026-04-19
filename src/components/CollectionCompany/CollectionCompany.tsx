5309b915e365533ada81da21cbfdd9ed0e604702
Ruslan Gadzhiev <rtgadzhiev@gmail.com>
2026-04-19 14:16:18 +0400

    fix: company when company is null

--- src/components/CollectionCompany/CollectionCompany.tsx
+++ src/components/CollectionCompany/CollectionCompany.tsx
@@ -4,10 +4,15 @@ import { InfoItem } from '../ui/InfoItem/InfoItem';
 
 export const CollectionCompany = () => {
   const { collection } = useCollection();
+  const title = collection?.company?.title;
+
+  if (!title) {
+    return null;
+  }
 
   return (
     <InfoGroup title="Компания">
-      <InfoItem title={collection?.company?.title} />
+      <InfoItem title={collection?.company?.title || 'Компания'} />
     </InfoGroup>
   );
 };
